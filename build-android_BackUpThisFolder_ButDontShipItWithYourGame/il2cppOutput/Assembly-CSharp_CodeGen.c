20051795c6db005ba78a3e80ec35a04cc83441e4
Jon Higgins <jon@jonhiggins.co.uk>
2023-03-21 14:45:11 +0000

    Swtich Unity version, add OpenXR

---
+++ build-android_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,169 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void KeepPlayingAudio::Awake()
+extern void KeepPlayingAudio_Awake_mF2E3B83B8CDB2919DA58E71F8D0D8D2C92E8237A (void);
+// 0x00000002 System.Void KeepPlayingAudio::.ctor()
+extern void KeepPlayingAudio__ctor_mDED54BC24DE113142417D67B7B6239EB1DF01449 (void);
+// 0x00000003 System.Void BallBehaviours::Start()
+extern void BallBehaviours_Start_m3613FEA126F34E35D5DCC6E8036355BFF09C12B5 (void);
+// 0x00000004 System.Void BallBehaviours::Update()
+extern void BallBehaviours_Update_mCC4820C605AEC1453A0DE8EE89A6CDBFA6E791F2 (void);
+// 0x00000005 System.Void BallBehaviours::OnCollisionEnter(UnityEngine.Collision)
+extern void BallBehaviours_OnCollisionEnter_mF20C1C0A3D725C7E1DB81B5C2836832E83BF7D6B (void);
+// 0x00000006 System.Void BallBehaviours::DestroySelf()
+extern void BallBehaviours_DestroySelf_mFF39C334737F060082D4062BF4060BBD75980AF3 (void);
+// 0x00000007 System.Void BallBehaviours::.ctor()
+extern void BallBehaviours__ctor_mB160081FAA0559345CC6FC949D0FC35B67F48838 (void);
+// 0x00000008 System.Void ControlBadDog::Start()
+extern void ControlBadDog_Start_m0EE341217D8E387683F0339896341D36CE9AC5BA (void);
+// 0x00000009 System.Void ControlBadDog::Update()
+extern void ControlBadDog_Update_mCE7BF737FAB881069FA950184E497AE84D648B20 (void);
+// 0x0000000A System.Void ControlBadDog::OnCollisionEnter(UnityEngine.Collision)
+extern void ControlBadDog_OnCollisionEnter_m6719AEBEFDD6C02E9C6AA267F8692F03F99B212C (void);
+// 0x0000000B System.Void ControlBadDog::DestroySelf()
+extern void ControlBadDog_DestroySelf_m0C4F8F9E9AACA3C295A81790EA678A0B123F30EE (void);
+// 0x0000000C System.Void ControlBadDog::.ctor()
+extern void ControlBadDog__ctor_m37A6B541938EA9968A3BBD26AEBEC48C380D2A2E (void);
+// 0x0000000D System.Void ControlPlayer::Start()
+extern void ControlPlayer_Start_mA1BD35621F70AEF633DE0782F07480E60D0FF7D9 (void);
+// 0x0000000E System.Void ControlPlayer::Update()
+extern void ControlPlayer_Update_m213C84EC6972134748A7BBCD370203AC2596300B (void);
+// 0x0000000F System.Void ControlPlayer::.ctor()
+extern void ControlPlayer__ctor_m7D99742BB3B5E4C55A0E0F2F418533935E41467B (void);
+// 0x00000010 System.Void FollowPlayer::Start()
+extern void FollowPlayer_Start_m138697E0740DB2570074B2DB69AE32AF46A994F9 (void);
+// 0x00000011 System.Void FollowPlayer::LateUpdate()
+extern void FollowPlayer_LateUpdate_m69B79F77CFC8072526CE0BA2D71179F66AE15B70 (void);
+// 0x00000012 System.Void FollowPlayer::.ctor()
+extern void FollowPlayer__ctor_mFEF642F6D296A2FBA30A01B637B6BD8DD2446114 (void);
+// 0x00000013 System.Void IntroScene::Start()
+extern void IntroScene_Start_mD22E92210F053C3014FF65C35291FB28547B5B52 (void);
+// 0x00000014 System.Collections.IEnumerator IntroScene::WaitThenLoadMainScene()
+extern void IntroScene_WaitThenLoadMainScene_mA5D64F4C0E92051B09638F3AEDCE7BCF02D0502E (void);
+// 0x00000015 System.Void IntroScene::.ctor()
+extern void IntroScene__ctor_mA2B7265AFF79FE63A4201222BA757F7626EDADD1 (void);
+// 0x00000016 System.Void IntroScene/<WaitThenLoadMainScene>d__1::.ctor(System.Int32)
+extern void U3CWaitThenLoadMainSceneU3Ed__1__ctor_m3D078263C03A86CF0633B6E88486C81C064FF6DE (void);
+// 0x00000017 System.Void IntroScene/<WaitThenLoadMainScene>d__1::System.IDisposable.Dispose()
+extern void U3CWaitThenLoadMainSceneU3Ed__1_System_IDisposable_Dispose_mA3F961ED1712B06AC0E87D1730456B9DB82E2C35 (void);
+// 0x00000018 System.Boolean IntroScene/<WaitThenLoadMainScene>d__1::MoveNext()
+extern void U3CWaitThenLoadMainSceneU3Ed__1_MoveNext_m82D363073F23AB99FD7094912D4B0D75672A5A5C (void);
+// 0x00000019 System.Object IntroScene/<WaitThenLoadMainScene>d__1::System.Collections.Generic.IEnumerator<System.Object>.get_Current()
+extern void U3CWaitThenLoadMainSceneU3Ed__1_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_mA353FEAEC358A279FC899823960F3D29C8904118 (void);
+// 0x0000001A System.Void IntroScene/<WaitThenLoadMainScene>d__1::System.Collections.IEnumerator.Reset()
+extern void U3CWaitThenLoadMainSceneU3Ed__1_System_Collections_IEnumerator_Reset_mBF32899F79359C152AC8A829B5D2E0776DA4EAA9 (void);
+// 0x0000001B System.Object IntroScene/<WaitThenLoadMainScene>d__1::System.Collections.IEnumerator.get_Current()
+extern void U3CWaitThenLoadMainSceneU3Ed__1_System_Collections_IEnumerator_get_Current_mDBA8FA917D774FF0D9AF41DF4387EA5B151103D4 (void);
+// 0x0000001C System.Void Score::Start()
+extern void Score_Start_m9851EAC634456AE4AD4D1163695A64EB3E4AA8FF (void);
+// 0x0000001D System.Void Score::UpdateScoreText()
+extern void Score_UpdateScoreText_m315BFC5DC4EE2EAEF06095E669688E5E10484F93 (void);
+// 0x0000001E System.Void Score::IncreaseScore(System.Int32)
+extern void Score_IncreaseScore_m4C2177073728B7E91B056552961EF247CC1201F6 (void);
+// 0x0000001F System.Void Score::DecreaseScore(System.Int32)
+extern void Score_DecreaseScore_mA05DC6EE71D0310A5A23FDE82DCED5EEB941EED0 (void);
+// 0x00000020 System.Void Score::.ctor()
+extern void Score__ctor_mDEDBA72E1F38C2F7CFF4E6C6556103C5D62C1670 (void);
+static Il2CppMethodPointer s_methodPointers[32] = 
+{
+	KeepPlayingAudio_Awake_mF2E3B83B8CDB2919DA58E71F8D0D8D2C92E8237A,
+	KeepPlayingAudio__ctor_mDED54BC24DE113142417D67B7B6239EB1DF01449,
+	BallBehaviours_Start_m3613FEA126F34E35D5DCC6E8036355BFF09C12B5,
+	BallBehaviours_Update_mCC4820C605AEC1453A0DE8EE89A6CDBFA6E791F2,
+	BallBehaviours_OnCollisionEnter_mF20C1C0A3D725C7E1DB81B5C2836832E83BF7D6B,
+	BallBehaviours_DestroySelf_mFF39C334737F060082D4062BF4060BBD75980AF3,
+	BallBehaviours__ctor_mB160081FAA0559345CC6FC949D0FC35B67F48838,
+	ControlBadDog_Start_m0EE341217D8E387683F0339896341D36CE9AC5BA,
+	ControlBadDog_Update_mCE7BF737FAB881069FA950184E497AE84D648B20,
+	ControlBadDog_OnCollisionEnter_m6719AEBEFDD6C02E9C6AA267F8692F03F99B212C,
+	ControlBadDog_DestroySelf_m0C4F8F9E9AACA3C295A81790EA678A0B123F30EE,
+	ControlBadDog__ctor_m37A6B541938EA9968A3BBD26AEBEC48C380D2A2E,
+	ControlPlayer_Start_mA1BD35621F70AEF633DE0782F07480E60D0FF7D9,
+	ControlPlayer_Update_m213C84EC6972134748A7BBCD370203AC2596300B,
+	ControlPlayer__ctor_m7D99742BB3B5E4C55A0E0F2F418533935E41467B,
+	FollowPlayer_Start_m138697E0740DB2570074B2DB69AE32AF46A994F9,
+	FollowPlayer_LateUpdate_m69B79F77CFC8072526CE0BA2D71179F66AE15B70,
+	FollowPlayer__ctor_mFEF642F6D296A2FBA30A01B637B6BD8DD2446114,
+	IntroScene_Start_mD22E92210F053C3014FF65C35291FB28547B5B52,
+	IntroScene_WaitThenLoadMainScene_mA5D64F4C0E92051B09638F3AEDCE7BCF02D0502E,
+	IntroScene__ctor_mA2B7265AFF79FE63A4201222BA757F7626EDADD1,
+	U3CWaitThenLoadMainSceneU3Ed__1__ctor_m3D078263C03A86CF0633B6E88486C81C064FF6DE,
+	U3CWaitThenLoadMainSceneU3Ed__1_System_IDisposable_Dispose_mA3F961ED1712B06AC0E87D1730456B9DB82E2C35,
+	U3CWaitThenLoadMainSceneU3Ed__1_MoveNext_m82D363073F23AB99FD7094912D4B0D75672A5A5C,
+	U3CWaitThenLoadMainSceneU3Ed__1_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_mA353FEAEC358A279FC899823960F3D29C8904118,
+	U3CWaitThenLoadMainSceneU3Ed__1_System_Collections_IEnumerator_Reset_mBF32899F79359C152AC8A829B5D2E0776DA4EAA9,
+	U3CWaitThenLoadMainSceneU3Ed__1_System_Collections_IEnumerator_get_Current_mDBA8FA917D774FF0D9AF41DF4387EA5B151103D4,
+	Score_Start_m9851EAC634456AE4AD4D1163695A64EB3E4AA8FF,
+	Score_UpdateScoreText_m315BFC5DC4EE2EAEF06095E669688E5E10484F93,
+	Score_IncreaseScore_m4C2177073728B7E91B056552961EF247CC1201F6,
+	Score_DecreaseScore_mA05DC6EE71D0310A5A23FDE82DCED5EEB941EED0,
+	Score__ctor_mDEDBA72E1F38C2F7CFF4E6C6556103C5D62C1670,
+};
+static const int32_t s_InvokerIndices[32] = 
+{
+	3445,
+	3445,
+	3445,
+	3445,
+	2814,
+	3445,
+	3445,
+	3445,
+	3445,
+	2814,
+	3445,
+	3445,
+	3445,
+	3445,
+	3445,
+	3445,
+	3445,
+	3445,
+	3445,
+	3353,
+	3445,
+	2796,
+	3445,
+	3293,
+	3353,
+	3445,
+	3353,
+	3445,
+	3445,
+	2796,
+	2796,
+	3445,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	32,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
